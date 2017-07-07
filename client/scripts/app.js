94606c1668edad7537136b199eae79837459fa27
Hack Reactor Student <student@hackreactor.com>
2017-07-07 12:34:13 -0700

    Render app object for app.js
    declared methods init through renderMessage
    Will revisit for creating rooms.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,46 @@
-// YOUR CODE HERE:
+var app = {
+  'server' : 'http://parse.la.hackreactor.com/chatterbox/classes/messages'
+};
+
+app.init = function() {};
+app.send = function(message) {
+  $.ajax({
+  // This is the url you should use to communicate with the parse API server.
+    url: 'http://parse.la.hackreactor.com/chatterbox/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+app.post = function() {};
+app.fetch = function(message) { 
+  $.ajax({
+  // This is the url you should use to communicate with the parse API server.
+    url: 'http://parse.la.hackreactor.com/chatterbox/classes/messages',
+    type: 'GET',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+app.clearMessages = function() {
+  //find the node
+  $('#chats').empty();
+};
+
+app.renderMessage = function(message) {
+  $('#chats').append("<div>" + message + "</div>");
+};
